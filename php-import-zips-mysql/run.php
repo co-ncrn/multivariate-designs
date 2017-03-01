b92536e4bc83e6d025fb7645126bfe7b5aa84c3e
Owen Mundy <omundy@gmail.com>
2017-03-01 09:21:17 -0500

    Adds auto-recognition for CSV/db table column names

--- php-import-zips-mysql/run.php
+++ php-import-zips-mysql/run.php
@@ -10,174 +10,141 @@ require 'inc/config.php';	 				// create database connection
 
 
 // testing
-$i = 0;
+$dirtests = 0;
+$tests = 0;
+$limit = 2;
 
 // get list of directories
 $path = '../../beckymasond.github.io/msa_data/';
 $dirs = scandir($path);
 
 // reporting
-$directories = $total_files = 0;
-$zip_files = $zips_missing = 0;
-$csv_files = $csvs_missing = 0;
+$directory_count = $total_file_count = 0;
+$zip_file_count = $zip_missing_count = 0;
+$csv_file_count = $csv_missing_count = 0;
 
+// ACS scenarios
+$scenarios = array(	"gen","hous","pov","trans");
 
 // loop through directories
 foreach ($dirs as $dir) {
+	// exclude these directories
 	if ($dir === '.' || $dir === '..' || $dir === '.DS_Store') continue;
-	$directories++;
-
-	// count sub-directories
+	
+	// count directories, sub-directories
+	$directory_count++; 
 	$subdirs = scandir($path.$dir);
-	print ++$i .". ". $dir ."/ [". count($subdirs)." files]\n";
-
-	// ACS scenarios
-	$scenarios = array(	"gen","hous","pov","trans");
+	print ++$dirtests .". ". $dir ."/ [". count($subdirs)." files]\n";
 
 	// look for ZIP ARCHIVE files in each $dir
 	foreach ($scenarios as $scenario){
 		
-		// file ref
+		// ZIP file path
 		$zip = $dir ."_". $scenario ."_results.zip";
 
 		// make sure file exists
 		if (!file_exists($path.$dir."/".$zip)) {
 			print "\t - ". $zip ." - ####### ZIP FILE MISSING ########"."\n";
-			$zips_missing++;
+			$zip_missing_count++;
 			continue;
 		}
-		$zip_files++;
-		$zArchive = new ZipArchive();
+		$zip_file_count++;
 		
-		// confirm zip exists
+		// confirm ZIP exists
+		$zArchive = new ZipArchive();
 		if($zArchive->open($path.$dir."/".$zip) !== false ){
 			print "\t - ". $zip . " [". $zArchive->numFiles ." files]\n";
 
-			// look for CSV files in each ZIP ARCHIVE
-
-
-			// file ref
-			$csvs = array(	"crosswalk" => $dir ."_". $scenario ."_crosswalk.csv",
-							"input_tracts" => $dir ."_". $scenario ."_input_tracts.csv",
-							"output_regions" => $dir ."_". $scenario ."_output_regions.csv");
+			// CSV file paths
+			$csv_filenames = array(	"crosswalk" => $dir ."_". $scenario ."_crosswalk.csv",
+									"input_tracts" => $dir ."_". $scenario ."_input_tracts.csv",
+									"output_regions" => $dir ."_". $scenario ."_output_regions.csv");
 			
+			// look for CSV files in each ZIP ARCHIVE
+			foreach ($csv_filenames as $type => $csv){
 
-			foreach ($csvs as $type => $csv){
+				// if file exists
 				if ($zArchive->locateName( $csv ) !== false){
 					print "\t\t - ". $csv ."\n";
-					$csv_files++;
-					$total_files ++;
+					$csv_file_count++;
+					$total_file_count++;
 
-					// csv file
-					$file = "zip://". $path.$dir."/".$zip ."#". $csv;
-
-					// import csv to array
-					$arr = array_map('str_getcsv', file($file));
-					
-					// remove header rows (1)
-					array_shift($arr);
+					$file = "zip://". $path.$dir."/".$zip ."#". $csv;	// define CSV file path inside ZIP
+					$arr = array_map('str_getcsv', file($file));		// import CSV to array
 					//print_r($arr);
+					$db_table_name = $dir."_".$scenario."_".$type;		// define db table name
 
-
-					// table name
-					$table = $dir."_".$scenario."_".$type;
-
-					if (!createTable($table,$type)) exit("\ncreateTable error");
-					if (!insertCSV($table,$arr,$type)) exit("\ninsertCSV error");
+					// create db table and insert CSV
+					$csv_col_names = returnCSVCols($arr[0]);
+					//if (!createTable($db_table_name,$type,$scenario,$csv_col_names)) exit("\ncreateTable error");
+					//if (!insertCSV($db_table_name,$arr,$type,$scenario,$csv_col_names)) exit("\ninsertCSV error");
 				
 
 
-print $table ."\n";
+					
 
-exit();
-
-
+					if (++$tests >= 3) exit("\n\n $tests tests done\n\n");
 
 				} else {
 					print "\t\t - ". " - ####### ". $csv ." CSV FILE MISSING ########"."\n";	
-					$csvs_missing++;
+					$csv_missing_count++;
 				}
 			}
 
-
 			 
+		} else {
+			exit("ZIP archive error");
 		}
 	}
-	
-
-
-
-
-
-/*
-
-
-	print $dir ."\n";
-	if(++$i > 0) exit();
-*/
 }
 
 
 
 /**
- *	insertData()
- *	@param String - $table 
- *	@param Array - $arr Array of data to insert 
- *	@param String - $type "crosswalk","input_tracts","output_regions" 
+ *	returnCSVCols()
+ *	@param  Array $header_row first line of CSV
+ *	@return Array $cols 
  */
-function insertCSV($table,$arr,$type){
-	
-	global $db;
-
-	// columns
-	if ($type == "crosswalk"){
-		$cols = Array("RID", "TID");
-	} else if ($type == "input_tracts"){
-		$cols = Array("RID", "TID");
-	} else if ($type == "output_regions"){
-		$cols = Array("RID", "TID");
+function returnCSVCols($header_row){
+	// loop through each column in first line of csv
+	$cols = array();
+	foreach ($header_row as $col){
+		if ($col == "TID" || $col == "RID" || substr($col, 0, 1) != "B" )
+			$cols[] = $col;
 	}
-	// insert
-	$result = $db->insertMulti($table, $arr, $cols);
-	if(!$result) {
-		echo "\ninsert failed: " . $db->getLastError();
-	} else {
-		echo "\ninserted with following id\'s: " . implode(', ', $result);
-	}
-
-	if ($db->getLastErrno() === 0)
-	    echo "\ninsertMulti succesfull";
-	else
-	    echo "\ninsertMulti failed. Error: ". $db->getLastError();
-
+	//print_r($cols);
+	return $cols;
 }
-
-
 
 /**
  *	createTable()
- *	@param String - $table 
+ *	@param String - $db_table_name 
  *	@param String - $type "crosswalk","input_tracts","output_regions" 
+ *	@param String - $scenario "gen","hous","pov","trans"
  */
-function createTable($table,$type){
-
+function createTable($db_table_name,$type,$scenario,$csv_col_names){
 	global $db;
+	print "\t\t - Creating db table: ". $db_table_name ."\n";
 
-	// drop table if it exists
-	$sql = "DROP TABLE IF EXISTS $table";
+	// drop db table if it exists
+	$sql = "DROP TABLE IF EXISTS $db_table_name";
 	$result = $db->rawQuery($sql);
-
+	// error checking
 	if ($db->getLastErrno() === 0) echo "\nDROP TABLE succesfull";
 	else echo "\nDROP TABLE failed. Error: ". $db->getLastError();
 
-	// determine table create syntax
+
+
+
+
+	// determine db table create syntax
 	if ($type == "crosswalk"){
-		$sql = "CREATE TABLE $table (
+		$sql = "CREATE TABLE $db_table_name (
 					`RID` int(11) DEFAULT NULL,
 					`TID` varchar(255) DEFAULT NULL
 				) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;";
 	} else if ($type == "input_tracts"){
-		$sql = "CREATE TABLE $table (
+		$sql = "CREATE TABLE $db_table_name (
 					`TID` varchar(255) DEFAULT NULL,
 					/*`B17006_016E` int(11) DEFAULT NULL,
 					`B25120_005M` varchar(255) DEFAULT NULL,
@@ -218,28 +185,64 @@ function createTable($table,$type){
 					`hsincrentCV` varchar(255) DEFAULT NULL
 				) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;";
 	} else if ($type == "output_regions"){
-		$sql = "CREATE TABLE $table (
+		$sql = "CREATE TABLE $db_table_name (
 				`RID` int(11) DEFAULT NULL,
 				`TID` varchar(255) DEFAULT NULL
 				) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;";
 	}
 	// run query
 	$result = $db->rawQuery($sql);
-
-	// check for errors
+	// error checking
 	if ($db->getLastErrno() === 0) echo "\nCREATE TABLE succesfull";
 	else echo "\nCREATE TABLE failed. Error: ". $db->getLastError();
 
 }
 
 
+/**
+ *	insertData()
+ *	@param String - $db_table_name 
+ *	@param Array - $arr Array of data to insert 
+ *	@param String - $type "crosswalk","input_tracts","output_regions" 
+ *	@param String - $scenario "gen","hous","pov","trans"
+ */
+function insertCSV($db_table_name,$arr,$type,$scenario,$csv_col_names){
+	
+	global $db;
+
+	// columns
+	if ($type == "crosswalk"){
+		$cols = Array("RID", "TID");
+	} else if ($type == "input_tracts"){
+		if ($scenario == "gen"){
+			$cols = Array("TID","");
+		} else if ($scenario == "hous"){
+			$cols = Array("xxx");
+		} else if ($scenario == "pov"){
+			$cols = Array("xxx");
+		} else if ($scenario == "trans"){
+			$cols = Array("xxx");
+		}
+	} else if ($type == "output_regions"){
+		$cols = Array("xxx");
+	}
+	// insert
+	$result = $db->insertMulti($db_table_name, $arr, $cols);
+	// error checking
+	if ($db->getLastErrno() === 0) echo "\nninsertMulti succesfull";
+	else echo "\nninsertMulti failed. Error: ". $db->getLastError();
+
+}
+
+
+
 
 
 // reporting
-print $directories ." directories\n";
-print $zip_files ." ZIP files (". $zips_missing ." missing)\n";
-print $csv_files ." CSV files (". $csvs_missing ." missing)\n";
-print $total_files ." total files\n";
+print $directory_count ." directory_count\n";
+print $zip_file_count ." ZIP files (". $zip_missing_count ." missing)\n";
+print $csv_file_count ." CSV files (". $csv_missing_count ." missing)\n";
+print $total_file_count ." total files\n";
 
 
 
