1ba1f478214f84dc2518033ef1d81890088dfa5d
Owen Mundy <omundy@gmail.com>
2017-03-01 12:38:29 -0500

    Adds README.md

--- server/index.js
+++ server/index.js
@@ -24,26 +24,7 @@ server.connection({
 	}
 });
 
-/*
 
-accept calls
-	get all data for an MSA + scenario
-		https://domain.com/api/msa/10180/gen/
-		https://domain.com/api/msa/{msa}/{scenario}/
-
-	get all data for a tract
-		https://domain.com/api/tract/g48441012300/gen/
-		https://domain.com/api/tract/{tid}/{scenario}/
-
-join the correct tables
-	10180_gen_crosswalk
-	10180_gen_input_tracts
-	10180_gen_output_regions
-
-reply
-	{ }
-
-*/
 
 
 // mysql
